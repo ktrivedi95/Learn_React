36538f90ef107496a929c6c0b6ff26c1b966d6b6
Kishan Trivedi <ktrivedi@emich.edu>
2019-09-16 11:59:16 -0400

    25 mins

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Kishan Trivedi</h1>
+            <p>I am learning React JS</p>
+            <ol>
+                <li>Europe</li>
+                <li>Asia</li>
+                <li>Australia</li>
+            </ol>
+        </div>
+    );
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
